f0573215a6a4bb08b201500b95b4ff1021a2a500
Calvin Che <cheziyi@gmail.com>
2024-08-02 23:52:10 +0800

    Readme and tweak main client (#6)

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Synnax Lab SDK
+
+## Installation
+
+```
+pip install synnax-lab-sdk
+```
+
+## Usage
+
+```
+from synnax_lab_sdk.client import SynnaxLabClient
+
+synnax_lab_client = SynnaxLabClient(api_key="your_api_key")
+files = synnax_lab_client.get_datasets()
+# Train your model and generate predictions
+synnax_lab_client.submit_predictions(files["dataset_date"], submissions_path)
+```
+
+Sample file also [available here](https://github.com/synnax-ai/synnax-lab-sdk/blob/master/src/samples/main.py)
--- src/samples/main.py
+++ src/samples/main.py
@@ -3,17 +3,15 @@ from synnax_lab_sdk.client import SynnaxLabClient
 
 def main():
     synnax_lab_client = SynnaxLabClient(api_key="your_api_key")
-    synnax_lab_client.get_datasets()
 
     files = synnax_lab_client.get_datasets()
 
     # TODO: Train your model and generate predictions
     submissions_path = files["sample_submission_path"]
 
-    synnax_lab_client.submit_predictions(submissions_path)
+    synnax_lab_client.submit_predictions(files["dataset_date"], submissions_path)
 
-    past_submissions = synnax_lab_client.get_past_submissions()
-    print(past_submissions)
+    synnax_lab_client.get_past_submissions()
 
 
 if __name__ == "__main__":
