848b4fe5e11f37ed33fec09a51d714e9f1a0d693
Calvin Che <cheziyi@gmail.com>
2024-08-02 18:46:39 +0800

    Add api clients

---
+++ src/synnax_lab_sdk/api_clients/dataset.py
@@ -0,0 +1,18 @@
+from typing import TypedDict, cast
+
+from synnax_lab_sdk.http_client.bearer_token import HttpBearerTokenClient
+
+
+class DownloadDatasetsResponse(TypedDict):
+    date: str
+    fileUrl: str
+    durationLeftForSubmissions: str
+
+
+class PublicCompanyDatasetClient:
+    def __init__(self, http_client: HttpBearerTokenClient):
+        self.http_client = http_client
+
+    def download_datasets(self) -> DownloadDatasetsResponse:
+        response = self.http_client.post("/datasets/download", {})
+        return cast(DownloadDatasetsResponse, response)
---
+++ src/synnax_lab_sdk/constants.py
@@ -0,0 +1,3 @@
+# TODO: Update to production values after release
+PUBLIC_COMPANY_DATASET_URL: str = ""
+PUBLIC_COMPANY_PREDICTION_SUBMISSION_URL: str = ""
